76bea608c789c01504fadf8ea08998b5c24b35a9
Wentrick <davi.w.feijo@gmail.com>
2023-04-17 08:14:18 -0300

    atualizacoes

--- Exercicios/dea_exercicios_lab_2.R
+++ Exercicios/dea_exercicios_lab_2.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse,car)
 # Exercicio 1
 
 
@@ -42,32 +42,77 @@ test_stat = (ssqtrat/(a-1))/(ssqres/(a*n-a))
 
 qf(0.05,(a-1),(a*n-a), lower.tail = FALSE)
 
-valor_p = pf()
-#auto
+valor_p = pf(test_stat,(a-1),(a*n-a),lower.tail = FALSE)
+
+#Pelo R
 
 dados_2 = dados %>%
   select(-7) %>%
   pivot_longer(cols = c(obs_1,obs_2,obs_3,obs_4,obs_5), values_to = "values", names_to = "nomes") %>%
   mutate(potencia = as.factor(potencia)) %>%
   select(-2)
-
+#anova
 res.aov <- aov(values ~ potencia, data = dados_2)
-# Summary of the analysis
+#sumário
 summary(res.aov)
 
 #1.3 - Os pressupostos necessarios foram atendidos
-dados_residuos = dados_2 %>%
-  pivot_wider(cols = potencia, values_from = values, names_from = potencia, names_prefix = "pot", names_sep = "_")
+dados_residuo <- dados %>%
+  mutate(mtrat = total/5) %>%
+  pivot_longer(cols = c(obs_1,obs_2,obs_3,obs_4,obs_5),
+               values_to = 'obs',names_to='rep') %>%
+  select(1,3,5) %>%
+  mutate(residuo = obs-mtrat)
   
+#normalidade por gráfico
+qqnorm(dados_residuo$residuo)
+qqline(dados_residuo$residuo)
 
-residuos = c(c(obs_1 - trat_medias$media[1]),c(obs_2 - trat_medias$media[2]),c(obs_3 - trat_medias$media[3]),c(obs_4 - trat_medias$media[4]),c(obs_5 - trat_medias$media[5]))
+
 #normalidade
-shapiro.test(dados_2$values)
+shapiro.test(dados_residuo$residuo)
+
+#independencia por gráfico
+
+plot(dados_residuo$residuo)
 
 #variancias (homscedasticidade)
 
-bartlett.test(dados_2$values ~ dados_2$potencia)
+#testeF (variancia)
+
+
+#teste de bartlett (parametrico)
+bartlett.test(dados_residuo$residuo ~ dados_residuo$potencia)
+
+#teste de levene
+
+
+
+leveneTest(y = dados_residuo$obs, group = dados_residuo$potencia)
 
 #1.4 - Qual sua conclusao sobre os resultados encontrados 
+
+#1.5 - Qual a proporcao da variacao total explicada pelo modelo ajustado no item 1.2?
+
+
+r_quadrado = ssqtrat/ssqtot
+
+#1.6 -Se a hipotese nula for rejeita, quais potencias de radio frequencia diferem entre si? Apresente as hipoteses que serao testadas e a estatıstica do teste.
+
+
+#teste de fisher (manual)
+n = 5
+N = 20
+test_stat = (max(trat_medias$media) - min(trat_medias$media))/sqrt((ssqres/(a*n-a))*(1/n + 1/n))
+
+
+qt((1-0.95)/2,N-1*sqrt((ssqres/(a*n-a))*(1/n + 1/n)))
+
+pt(test_stat,N-1*sqrt((ssqres/(a*n-a))*(1/n + 1/n)),lower.tail = FALSE)
+
+#teste de tukey (manual)
+
+
+
 
 
