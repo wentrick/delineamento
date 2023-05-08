9854dbc00518f75568191afc8dab0407c552c6a6
Wentrick <davi.w.feijo@gmail.com>
2023-05-08 15:16:02 -0300

    atualizaçoes finais

--- Exercicios/exercicio_aula13/exercicio_aula13_script.R
+++ Exercicios/exercicio_aula13/exercicio_aula13_script.R
@@ -58,13 +58,13 @@ f_crit_bloco = qf(alfa,glb,glr)
 
 #p-valor observado
 
-f_value = round(pf(f_obs,glt,glr,lower.tail = FALSE),3)
+f_value = round(pf(f_obs,glt,glr,lower.tail = FALSE),5)
 
-f_value_blocos = round(pf(f_obs_blocos,glb,glr,lower.tail = FALSE),7)
+f_value_blocos = round(pf(f_obs_blocos,glb,glr,lower.tail = FALSE),6)
 
 
 # Tabela da ANOVA
-anova_table <- data.frame(Fonte_de_variacao = c("Produto", "Rolo de tecido", "Residuos", "Total"),
+anova_table <- data.frame(Fonte_de_variacao = c("Marca", "Raça", "Residuos", "Total"),
                           GL = c(glt, glb, glr, gltot),
                           SS = c(ssqtrat, ssqblocos, ssqres, ssqtot),
                           MQ = c(round(qmtrat,2), round(qmbloco,2), round(qmres,2), ''),
@@ -103,15 +103,35 @@ var_exp = 1 - (ssqres/ssqtot)
 
 cat("A variancia explicada pelo medole é", var_exp)
 
+var_exp = ((ssqtrat+ssqblocos)/ssqtot)
 
+cat("A variancia explicada pelo medole é", var_exp)
 
 # 1.5) Considerando que o objetivo do experimento e maximizar a variavel resposta, 
 # qual e o elemento quımico que deve ser recomendado? Use teste de Tukey para subsidiar sua resposta.
 
+alfa = 0.05
 
+q.value <- qtukey(alfa, t, glr, lower.tail = F)
 
+hsd = q.value * sqrt(qmres/b)
 
-# 1.6) Refa¸ca as contas necessarias para responder os itens (1.2), (1.3) e (1.6) utilizando as funcoes
+round(2*ptukey(5.3, t, glr, lower.tail = F),4)
+
+combinacoes = combn(unique(dados$marcas),2)
+comb_diff = combn(trat_media,2)
+
+diferencas <- data.frame(
+  comparacao = apply(combinacoes, 2, paste0, collapse = "-"),
+  diferenca = apply(comb_diff, 2, diff)) %>%
+  mutate(lwr = diferenca - q.value * sqrt(qmres/b),
+         upr = diferenca + q.value * sqrt(qmres/b),
+         pvalor = round(ptukey(abs(diferenca/(sqrt(qmres/b))), t, glr, lower.tail = F),6),
+         hsd = abs(diferenca) >= hsd)
+
+diferencas
+
+# 1.6) Refaca as contas necessarias para responder os itens (1.2), (1.3) e (1.6) utilizando as funcoes
 #aov e TukeyHSD e confira com os resultados obtidos.
 
 
@@ -129,9 +149,9 @@ TukeyHSD(aov_res)
 
 kruskal.test(dados_long$values ~ dados_long$marcas)
 
-# 1.7) Determine a probabilidade do erro tipo 2 para o caso de: (τ1 = −1.5, τ2 = 0, τ3 = 0, τ4 = 1.5).
+# 1.7) Determine a probabilidade do erro tipo 2 para o caso de: (τ1 = −5, τ2 = 0, τ3 = 0, τ4 = 0, τ5 = 5).
 alpha = 0.05
-taui = c(-1.5,0,0,1.5)
+taui = c(-5,0,0,0,5)
 
 sigma2 = qmres
 
@@ -144,8 +164,19 @@ beta = pf(q = fcrit,df1 = t-1,df2 = (t-1)*(b-1), ncp = delta)
 poder = 1 - beta
 poder
 
-# 1.8) Para os valores de taus considerados no item anterior, determine qual deve ser o n´umero de
+# 1.8) Para os valores de taus considerados no item anterior, determine qual deve ser o numero de
 #blocos para que o erro tipo 2 seja inferior a 10%?
 
-#vamos aumentando o numero de blocos (b) ate mudar a porcentagem
+b = 6
+
+delta = b*sum(taui^2/sigma2)
+
+fcrit = qf(1-alpha,t-1,(t-1)*(b-1))
+
+beta = pf(q = fcrit,df1 = t-1,df2 = (t-1)*(b-1), ncp = delta)
+
+poder = 1 - beta
+poder
+
+
 
