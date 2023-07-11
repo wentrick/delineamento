61c4d1f7e089ed5852b1c8d145b78efd27f8a672
Wentrick <davi.w.feijo@gmail.com>
2023-07-10 21:54:24 -0300

    lista 25

--- Exercicios/exercicios_aula25/exercicios_aula25_script.R
+++ Exercicios/exercicios_aula25/exercicios_aula25_script.R
@@ -17,7 +17,7 @@ dados = data.frame(matriz_combinacoes,rep_1,rep_2,rep_3) %>%
   mutate(total = rep_1 + rep_2 + rep_3)
 
 
-
+n = 3 #numero de repeticoes
 
 #calculando os efeitos
 
@@ -30,38 +30,45 @@ ac = dados$total[6]
 bc = dados$total[7]
 abc = dados$total[8]
 
-A = (a+ab+ac+abc-um-a-c-ac)/(4*n)
+contrasteA = (a+ab+ac+abc-um-b-c-bc)
+contrasteB = (b+ab+bc+abc-um-a-c-ac)
+contrasteC = (c+ac+bc+abc-um-a-b-ab)
+contrasteAB = (um-a-b+ab+c-ac-bc+abc)
+contrasteAC = (um-a+b-ab-c+ac-bc+abc)
+contrasteBC = (um+a-b-ab-c-ac+bc+abc)
+contrasteABC = (abc-ac-bc+c-ab+a+b-um)
 
-B = (b+ab+bc+abc-um-a-c-ac)/(4*n)
+A = contrasteA/(4*n)
 
-C = (c+ac+bc+abc-um-a-b-ab)/(4*n)
+B = contrasteB/(4*n)
 
-AB = (um-a-b+ab+c-ac-bc+abc)/(4*n)
+C = contrasteC/(4*n)
 
-AC = (um-a+b-ab-c-ac+bc+abc)/(4*n)
+AB = contrasteAB/(4*n)
 
-BC = (um+a-b-ab-c-ac+bc+abc)/(4*n)
+AC = contrasteAC/(4*n)
 
-ABC = ((A-1)*(B-1)*(C-1))/(4*n)
+BC = contrasteABC/(4*n)
+
+ABC = contrasteABC/(4*n)
 
 #formatando o banco para futuras analises
 
 dados_padronizado = dados %>%
-  pivot_longer(cols = c(rep_1,rep_2,rep_3), values_to = "values", names_to = "rep") %>%
-  mutate(produtos = as.factor(produtos)) 
+  pivot_longer(cols = c(rep_1,rep_2,rep_3), values_to = "values", names_to = "rep") 
   
 
 #soma de quadrados
 
-SQA = (A^2)/(8*n)
-SQB = (B^2)/(8*n)
-SQC = (C^2)/(8*n)
-SQAB = (AB^2)/(8*n)
-SQAC = (AC^2)/(8*n)
-SQBC = (BC^2)/(8*n)
-SQABC = (ABC^2)/(8*n)
+SQA = (contrasteA^2)/(8*n)
+SQB = (contrasteB^2)/(8*n)
+SQC = (contrasteC^2)/(8*n)
+SQAB = (contrasteAB^2)/(8*n)
+SQAC = (contrasteAC^2)/(8*n)
+SQBC = (contrasteBC^2)/(8*n)
+SQABC = (contrasteABC^2)/(8*n)
 SQtot = sum((dados_padronizado$values - mean(dados_padronizado$values))^2)
-SQres = SQtot - (SQA+SQB+SQAB+SQAC+SQBC+SQABC)
+SQres = SQtot - (SQA+SQB+SQC+SQAB+SQAC+SQBC+SQABC)
 
 #graus de liberdade
 a = length(unique(dados_padronizado$A))
@@ -77,7 +84,7 @@ glAC <- (a-1)*(c-1)
 glBC <- (b-1)*(c-1)
 glABC <- (a-1)*(b-1)*(c-1)
 glRES <- a*b*c*(r-1)
-
+glTOT = (8*n-1)
 #quadrados medios
 
 qmA = SQA/glA
@@ -92,29 +99,42 @@ qmres = SQres/glRES
 
 # valor F observado
 
-f_obs = qmtrat/qmres
-f_obs_blocos = qmbloco/qmres
-
-#valor critico de 5%
-alfa = 0.05
-f_crit = qf(alfa,glt,glr)
-f_crit_bloco = qf(alfa,glb,glr)
+f_obs_A = qmA/qmres
+f_obs_B = qmB/qmres
+f_obs_C = qmC/qmres
+f_obs_AB = qmAB/qmres
+f_obs_AC = qmAC/qmres
+f_obs_BC = qmBC/qmres
+f_obs_ABC = qmABC/qmres
 
 #p-valor observado
 
-f_value = round(pf(f_obs,glt,glr,lower.tail = FALSE),3)
-
-f_value_blocos = round(pf(f_obs_blocos,glb,glr,lower.tail = FALSE),7)
-
+f_value_A = round(pf(f_obs_A,glA,glRES,lower.tail = FALSE),7)
+f_value_B = round(pf(f_obs_B,glB,glRES,lower.tail = FALSE),7)
+f_value_C = round(pf(f_obs_C,glC,glRES,lower.tail = FALSE),7)
+f_value_AB = round(pf(f_obs_AB,glAB,glRES,lower.tail = FALSE),7)
+f_value_AC = round(pf(f_obs_AC,glAC,glRES,lower.tail = FALSE),7)
+f_value_BC = round(pf(f_obs_BC,glBC,glRES,lower.tail = FALSE),7)
+f_value_ABC = round(pf(f_obs_ABC,glABC,glRES,lower.tail = FALSE),7)
 
 # Tabela da ANOVA
-anova_table <- data.frame(Fonte_de_variacao = c("Produto", "Rolo de tecido", "Residuos", "Total"),
-                          GL = c(glt, glb, glr, gltot),
-                          SS = c(ssqtrat, ssqblocos, ssqres, ssqtot),
-                          MQ = c(round(qmtrat,2), round(qmbloco,2), round(qmres,2), ''),
-                          F = c(round(f_obs,3),round(f_obs_blocos,3),'',''),
-                          Pf = c(f_value, f_value_blocos, "", ''),
+anova_table <- data.frame(Fonte_de_variacao = c("Fator A", "Fator B", "Fator C", "A:B", "A:C", "B:C","A:B:C","Residuo","Total"),
+                          GL = c(glA, glB, glC, glAB, glAC, glBC, glABC, glRES, glTOT),
+                          SS = round(c(SQA, SQB, SQC, SQAB, SQAC, SQBC, SQABC, SQres, SQtot),1),
+                          MQ = round(c(qmA, qmB, qmC, qmAB, qmAC, qmBC, qmABC, qmres,0),1),
+                          F = round(c(f_obs_A, f_obs_B, f_obs_C, f_obs_AB, f_obs_AC, f_obs_BC, f_obs_ABC,0,0),1),
+                          Pf = round(c(f_value_A, f_value_B, f_value_C, f_value_AB, f_value_AC, f_value_BC, f_value_ABC,0,0),1),
                           stringsAsFactors = FALSE)
 rownames(anova_table) <- NULL
 
+
 anova_table
+
+
+modelo = aov(values~A*B*C, data = dados_padronizado)
+summary(modelo)
+
+
+
+
+
